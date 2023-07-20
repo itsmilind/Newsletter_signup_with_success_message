5f46ecb3127510ee1880cf0f92b23b2f7189e79c
Milind Mali <98891861+itsmilind@users.noreply.github.com>
2023-07-20 16:13:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+let container = document.querySelector(".container");
+let form = document.querySelector("form")
+let small = document.querySelector("small")
+let email = document.querySelector("form .email")
+let modalBtn = document.querySelector(".modal button");
+let modal = document.querySelector(".modal");
+let parag = document.querySelector(".modal span");
+
+
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    modal.style.display ="block";
+    container.style.display ="none";
+    email.value = "";
+})
+
+modalBtn.addEventListener("click", function(){
+    window.location.reload();
+})
+
+email.addEventListener("change", function(){
+    parag.innerHTML = email.value;
+    let val = email.value;
+    if ((!val.includes('@')) || (!val.includes('.com'))){
+        email.style.border = "1px solid hsl(4, 100%, 67%)";
+        email.style.backgorundcolor = "hsl(3, 100%, 93%)";
+        email.style.color = "hsl(4, 100%, 67%)";
+        small.style.dispaly = "inline"
+    }else{
+        email.style.border = "1px solid hsl(235, 18%, 26%)";
+        email.style.backgorund = "unset";
+        email.style.color  = "hsl(235, 18%, 26%)";
+        small.style.dispaly = "none";
+    }
+
+})
+
+
